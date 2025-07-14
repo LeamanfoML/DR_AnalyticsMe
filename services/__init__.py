7df5438b50ef6cd29b1256e8739a352ea05a7d4a
LeamanfoML <dezveer2@gmail.com>
2025-07-15 00:52:51 +0300

    Delete services directory

--- services/__init__.py
+++
@@ -1,6 +0,0 @@
-# Экспорт основных компонентов сервисного слоя
-from .api_client import PortalsAPI, TonnelAPI
-from .arbitrage import ArbitrageCalculator
-from .utils import calculate_profit
-
-__all__ = ['PortalsAPI', 'TonnelAPI', 'ArbitrageCalculator', 'calculate_profit']
--- services/utils.py
+++
@@ -1,4 +0,0 @@
-def calculate_profit(buy_price, sell_price, buy_commission, sell_commission, transfer_fee):
-    net_sell_price = sell_price * (1 - sell_commission)
-    total_buy_cost = buy_price * (1 + buy_commission) + transfer_fee
-    return net_sell_price - total_buy_cost
